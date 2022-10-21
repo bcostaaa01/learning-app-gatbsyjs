26ae1bb76bd56c15046821b036f89c71117623c0
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-10-21 12:24:20 +0100

    Format code, install coreui, add content to basics

---
+++ src/components/topic-card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  CCardBody,
+  CButton,
+  CCard,
+  CCardTitle,
+  CCardImage,
+} from "@coreui/react";
+
+export const TopicCard = () => {
+  return (
+    <div>
+      <CCard style={{ width: "18rem", fontFamily: "sans-serif" }}>
+        <CCardImage
+          style={{ height: "240px" }}
+          src="https://www.thoughtco.com/thmb/ZBTtHM-EBdcwG9YlaeQapyh6jYI=/1500x1000/filters:fill(auto,1)/Businesswoman-Greeting-58d042b45f9b581d7210dcfb.jpg"
+        />
+        <CCardBody>
+          <CCardTitle>Greetings</CCardTitle>
+          <CButton href="https://www.youtube.com/watch?v=ocPYue7Z-jY">
+            Watch video
+          </CButton>
+        </CCardBody>
+      </CCard>
+      <CCard style={{ width: "18rem", fontFamily: "sans-serif" }}>
+        <CCardImage
+          style={{ height: "240px" }}
+          src="https://tse4.mm.bing.net/th?id=OIP.VITbMFwOOyZhOREnIANbOgHaFj&pid=Api"
+        />
+        <CCardBody>
+          <CCardTitle>Introducing yourself</CCardTitle>
+          <CButton href="https://www.youtube.com/watch?v=UnEmEbWytI8">
+            Watch video
+          </CButton>
+        </CCardBody>
+      </CCard>
+    </div>
+  );
+};
+
+export default TopicCard;
